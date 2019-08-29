8b2e7e0855889840384ae0ae403d30cbdfb31578
Joshua Uyi <joshua.uyi@interswitchgroup.com>
2019-08-29 16:56:35 +0100

    added template and reactive forms

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,21 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { EventService } from './shared/event.service'
+import { ToastrService } from '../common/toastr.service'
+import { ActivatedRoute } from '@angular/router'
+import { IEvent } from './shared/index'
 
 @Component({
-    selector: 'events-list',
-    templateUrl: './events-list.component.html'
+  template: `
+  <div>
+    <h1>Upcoming Angular 2 Events</h1>
+    <hr/>
+    <div class="row">
+      <div *ngFor="let event of events" class="col-md-5">
+        <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
+      </div>
+    </div>
+  </div>
+  `
 })
-export class EventsListComponent {
-    event = {
-        id: 1,
-        name: 'Angular Connect',
-        date: '9/26/2036',
-        time: '10:00 am',
-        price: 599.99,
-        imageUrl: '/assets/images/angularconnect-shield.png',
-        location: {
-            address: '1057 DT',
-            city: 'London',
-            country: 'England'
-        }
-    }
+export class EventsListComponent implements OnInit {
+  events:IEvent[]
+
+  constructor(private eventService: EventService, private toastr: ToastrService, private route:ActivatedRoute) {
+    
+  }
+
+  ngOnInit() {
+    this.events = this.route.snapshot.data['events']
+  }
+
+  handleThumbnailClick(eventName) {
+    this.toastr.success(eventName)
+  }
 }
